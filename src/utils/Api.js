ae40807563380f5e059bfdbe258211089e0cda11
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-04-21 12:30:07 +0300

    fix

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -12,7 +12,7 @@ class Api {
         }
     }
 
-    getProfile() {
+    getUserInfo() {
         return fetch(`${this._baseUrl}/users/me`, {
                 headers: this._headers
             })
